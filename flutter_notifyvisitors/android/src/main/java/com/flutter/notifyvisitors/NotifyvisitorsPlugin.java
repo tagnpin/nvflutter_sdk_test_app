d6dc596277b8dc84621eabfeb202b94e94c9d029
tagnpinAndroid <deepa.m@notifyvisitors.com>
2026-01-31 14:29:57 +0530

    android sdk updated

--- flutter_notifyvisitors/android/src/main/java/com/flutter/notifyvisitors/NotifyvisitorsPlugin.java
+++ flutter_notifyvisitors/android/src/main/java/com/flutter/notifyvisitors/NotifyvisitorsPlugin.java
@@ -60,7 +60,7 @@ public class NotifyvisitorsPlugin extends BroadcastReceiver implements FlutterPl
     private MethodChannel channel;
 
     private static final String TAG = "NotifyVisitors-Flutter";
-    private static final String PLUGIN_VERSION = "1.4.1";
+    private static final String PLUGIN_VERSION = "1.5.0";
 
     private Context flutterContext;
     private Activity mainActivity;
